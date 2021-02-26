2313b2bab496b5e207048fe0b38f92a6b0eae5da
Jhonson Trazona <jhonson_trazona@ligph.com>
2021-02-26 18:06:12 +0800

    Implement single page 80%

--- src/page/SinglePage.js
+++ src/page/SinglePage.js
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom'
 import EyeCatch from '../img/blogpost-mv.png';
 
 const SinglePage = props => {
-  const { id, title, published, content, comments } = props.location.state
+  const { title, published, content, comments } = props.location.state
+
+  console.log(comments)
+
   return (
     <>
       <nav className="breadcrumbs">
         <div className="breadcrumbs-inner l-container">
-          <ul class="breadcrumbs__list">
+          <ul className="breadcrumbs__list">
             <li className="breadcrumbs__list-item"><Link to='/'>Home</Link></li>
             <li className="breadcrumbs__list-item">{title}</li>
           </ul>
@@ -18,12 +21,36 @@ const SinglePage = props => {
       <section className="l-container single">
         <div className="single__meta">
           <div className="single__published">{published}</div>
-          <div className="single__title">{title}</div>
+          <h2 className="single__title">{title}</h2>
         </div>
 
-        <div className="single__eyecacth" style={{ backgroundImage: `url(${EyeCatch})`}}></div>
+        <div className="single__eyecatch" style={{ backgroundImage: `url(${EyeCatch})`}}></div>
 
-        <div className="single__content">{content}</div>
+        <div className="single__content">
+          {content.map(({text}, i) => (
+            <p key={i}>{text}</p>
+          ))}
+        </div>
+
+        <div className="comments">
+          <h3 className="comments__head">COMMENTS</h3>
+          <div className="comments__container">
+            <ul className="comments__list">
+              {comments.map(({body, createdAt},i)=>(
+                <li className="comments__list-item">
+                  <p className="comments__body">{body}</p>
+                  <div className="comments__date">{createdAt}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="comments__form">
+            <form>
+              <textarea placeholder="Write Comment"></textarea>
+              <input class="btn btn--submit" type="submit" value="submit" />
+            </form>
+          </div>
+        </div>
       </section>
     </>
   )
